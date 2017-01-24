ff962db43450a7a43346ea8ac03052c275b0e987
cscoi011 <wnstkdyu@gmail.com>
2017-01-24 16:14:11 +0900

    commit

---
+++ project/SleepStudy/SleepStudy/TimeTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  TimeTableViewController.swift
+//  SleepStudy
+//
+//  Created by cscoi011 on 2017. 1. 24..
+//  Copyright © 2017년 KKUMA. All rights reserved.
+//
+
+import UIKit
+
+class TimeTableViewController: UITableViewController {
+
+    
+    let subjects:[Subject] = [Subject(name: "조직행동론",prof: "전재욱",place: "LP 504호",time:[(0,840,915),(2,840,915)]),
+                              Subject(name: "네트워크",prof: "민성기",place: "정보관 202호",time:[(1,540,615),(3,540,615)]),
+                              Subject(name: "교직실무",prof: "김재덕",place: "교욱관 203호",time:[(0,600,730),(4,600,730)])]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        //여기에 추가
+        
+        subjects[0].records += [Record(path: "/rec/file1.mov",date: "3월 2일"),
+                                Record(path: "/rec/file2.mov",date: "3월 4일"),
+                                Record(path: "/rec/file3.mov",date: "3월 9일")]
+        
+        subjects[1].records += [Record(path: "/rec/file4.mov",date: "3월 3일"),
+                                Record(path: "/rec/file5.mov",date: "3월 5일"),
+                                Record(path: "/rec/file6.mov",date: "3월 10일")]
+        
+        subjects[2].records += [Record(path: "/rec/file7.mov",date: "3월 2일"),
+                                Record(path: "/rec/file8.mov",date: "3월 6일"),
+                                Record(path: "/rec/file9.mov",date: "3월 9일")]
+        
+        subjects[0].records[0].memos += [Memo(content: "메모 내용",type: "메모 종류", time:1000)]
+        
+        subjects[0].records[0].captures += [Capture(path: "경로", time:1000)]
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return subjects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "timeCell", for: indexPath)
+
+        // Configure the cell...
+
+        let num = indexPath.row
+        cell.textLabel?.text = subjects[num].name
+        cell.detailTextLabel?.text = subjects[num].place
+        
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        
+        let destVC = segue.destination as! RecordTableViewController
+        let selectedIndex = self.tableView.indexPathForSelectedRow?.row
+        let selected = subjects[selectedIndex!]
+        
+        destVC.selectedSubject = selected
+    }
+    
+
+}
--- project/SleepStudy/SleepStudy/ViewController.swift
+++ project/SleepStudy/SleepStudy/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,3 +24,71 @@ class ViewController: UIViewController {
 
 }
 
+class Subject{
+    
+    
+    
+    var name:String = ""
+    var prof:String = ""
+    var place:String = ""
+    
+    var time:[(day:Int,startTime:Int,endTime:Int)]
+    var records:Array<Record> = []
+    
+    init(name: String, prof: String, place: String, time:[(day:Int,startTime:Int,endTime:Int)]){
+        self.name = name
+        self.prof = prof
+        self.place = place
+        self.time = time
+    }
+}
+
+class Memo{
+    var content:String
+    var type:String
+    let time:Int
+    
+    init(content: String, type:String, time:Int){
+        self.content = content
+        self.type = type
+        self.time = time
+    }
+}
+
+class Capture{
+    var path:String
+    let time:Int
+    
+    init(path:String, time:Int){
+        self.path = path
+        self.time = time
+    }
+}
+
+class Record{
+    
+    var path:String
+    var memos:Array<Memo>
+    var captures:Array<Capture>
+    let date:String
+    
+    init(path:String, date:String){
+        self.path = path
+        self.date = date
+        self.memos = []
+        self.captures = []
+    }
+}
+
+class Setting{
+    
+    var password:Data
+    var theme:String
+    var memotypes:Array<String>
+    
+    init(){
+        password = Data()
+        theme = ""
+        memotypes = []
+    }
+}
