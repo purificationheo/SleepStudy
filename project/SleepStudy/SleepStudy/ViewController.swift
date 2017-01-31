59b8ccf0d732f1ce032775ed69e40b9e9e18b7c3
wnstkdyu <wnstkdyu@gmail.com>
2017-01-31 17:49:26 +0900

    camera 기능 구현

--- project/SleepStudy/SleepStudy/ViewController.swift
+++ project/SleepStudy/SleepStudy/ViewController.swift
@@ -8,21 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UIPopoverControllerDelegate,UINavigationControllerDelegate {
 
+    var picker:UIImagePickerController?=UIImagePickerController()
     
+    @IBOutlet weak var imgaeView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        picker?.delegate=self
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func TakePhoto(_ sender: Any) {
+        openCamera ()
+    }
+    
+    func openCamera(){
+        
+    if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)){
+    picker!.allowsEditing = false
+    picker!.sourceType = UIImagePickerControllerSourceType.camera
+    picker!.cameraCaptureMode = .photo
+    present(picker!, animated: true, completion: nil)
+    }else{
+    let alert = UIAlertController(title: "Camera Not Found", message: "This device has no Camera", preferredStyle: .alert)
+    let ok = UIAlertAction(title: "OK", style:.default, handler: nil)
+    alert.addAction(ok)
+    present(alert, animated: true, completion: nil)
+    }
+    
+    }
 }
+
+
 
 class Subject{
     
