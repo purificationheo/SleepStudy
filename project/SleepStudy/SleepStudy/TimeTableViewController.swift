256a2c4998b94862a6df3274601bb867a85a9723
clay4952 <cscoi030@cscoi030ui-iMac.local>
2017-02-06 19:01:36 +0900

    s
    s

--- project/SleepStudy/SleepStudy/TimeTableViewController.swift
+++ project/SleepStudy/SleepStudy/TimeTableViewController.swift
@@ -128,7 +128,7 @@ class TimeTableViewController: UITableViewController {
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
+     // Return false if you do not want the specified item to be editable.
         return true
     }
     */
