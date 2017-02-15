ecf3cb02a1b75816c208bf32290a258fce7e332e
purification418 <ini03087@naver.com>
2017-02-15 14:45:05 +0900

    0215_

--- project/SleepStudy/SleepStudy/SubjectAddViewController.swift
+++ project/SleepStudy/SleepStudy/SubjectAddViewController.swift
@@ -10,48 +10,40 @@ import UIKit
 
 class SubjectAddViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
    
-    let pickerData = [["월","화","수","목","금"],["오전", "오후"],["0","1","2","3","4","5","6","7","8","9","10","11","12"],["0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"],["0","1","2","3","4","5","6","7","8","9","10","11","12"],["0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]]
+    let timePicker = UIPickerView()
+    let timePicker2 = UIPickerView()
+    
+    let pickerData = [["월","화","수","목","금"],["오전", "오후"],["1","2","3","4","5","6","7","8","9","10","11","12"],["0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"],["오전", "오후"],["0","1","2","3","4","5","6","7","8","9","10","11","12"],["0", "5", "10", "15", "20", "25", "30", "35", "40", "45", "50", "55"]]
 
    
     
-    @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var nameText: UITextField!
     @IBOutlet weak var profText: UITextField!
     @IBOutlet weak var placeText: UITextField!
     @IBOutlet weak var timePickerText: UITextField!
-
-    @IBAction func monday(_ sender: Any) {
-    }
-   
-    @IBAction func thusday(_ sender: Any) {
-    }
-    @IBAction func wensday(_ sender: Any) {
-    }
-    @IBAction func tusday(_ sender: Any) {
-    }
-    @IBAction func friday(_ sender: Any) {
-    }
+    @IBOutlet weak var timePickerText2: UITextField!
     
-    @IBAction func SaveButton(_ sender: Any) {
+      @IBAction func SaveButton(_ sender: Any) {
         
         var newSubject = Subject(name: "" , prof: "" , place: "" ,time: [(day:0,startTime:0,endTime:0)])
         
         newSubject.name = nameText.text!
         newSubject.prof = profText.text!
         newSubject.place = placeText.text!
-      
         
     }
     
-    @IBOutlet weak var timePicker: UIPickerView!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
         
         timePicker.delegate = self
         timePicker.dataSource = self
+        
+        timePicker2.delegate = self
+        timePicker2.dataSource = self
+        
+        timePickerText.inputView = timePicker
+        timePickerText2.inputView = timePicker2
         
         // Do any additional setup after loading the view.
     }
@@ -61,7 +53,7 @@ class SubjectAddViewController: UIViewController, UIPickerViewDelegate, UIPicker
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 6
+        return 7
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
@@ -75,30 +67,39 @@ class SubjectAddViewController: UIViewController, UIPickerViewDelegate, UIPicker
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int,
         inComponent component: Int)
     {
-       updateLabel()
-    }
-
-    func updateLabel(){
+        if pickerView == timePicker {
+        
         let day = pickerData[0][timePicker.selectedRow(inComponent: 0)]
         let ampm = pickerData[1][timePicker.selectedRow(inComponent: 1)]
         let starthour = pickerData[2][timePicker.selectedRow(inComponent: 2)]
         let startminute = pickerData[3][timePicker.selectedRow(inComponent: 3)]
-        let endhour = pickerData[4][timePicker.selectedRow(inComponent: 4)]
-        let endminute = pickerData[5][timePicker.selectedRow(inComponent: 5)]
-        timeLabel.text = "\(day)요일      " +
-            "\(ampm)    " + " \(starthour)시  " + "\(startminute)분" + "~  \(endhour)시" + "   \(endminute)분"
+        let ampm2 = pickerData[4][timePicker.selectedRow(inComponent: 4)]
+        let endhour = pickerData[5][timePicker.selectedRow(inComponent: 5)]
+        let endminute = pickerData[6][timePicker.selectedRow(inComponent: 6)]
         
+       return timePickerText.text =  "\(day)요일  " + "\(ampm) " + " \(starthour)시  " + "\(startminute)분" + " ~ \(ampm2)  " + "\(endhour)시  " + "\(endminute)분"
+            
+        } else if pickerView == timePicker2 {
+        let day = pickerData[0][timePicker2.selectedRow(inComponent: 0)]
+        let ampm = pickerData[1][timePicker2.selectedRow(inComponent: 1)]
+        let starthour = pickerData[2][timePicker2.selectedRow(inComponent: 2)]
+        let startminute = pickerData[3][timePicker2.selectedRow(inComponent: 3)]
+        let ampm2 = pickerData[4][timePicker2.selectedRow(inComponent: 4)]
+        let endhour = pickerData[5][timePicker2.selectedRow(inComponent: 5)]
+        let endminute = pickerData[6][timePicker2.selectedRow(inComponent: 6)]
+        
+       return timePickerText2.text =  "\(day)요일  " + "\(ampm) " + " \(starthour)시  " + "\(startminute)분" + " ~ \(ampm2)  " + "\(endhour)시  " + "\(endminute)분"
+    }
         
     }
     
-        /*
-    // MARK: - Navigation
+           // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-    }
-    */
+    
+}
 
 }
